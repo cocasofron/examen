8c21aa536700ecc90b777148d2ad05021ab7ae5f
corinasofron1 <corinasofron1@gmail.com>
2019-06-28 23:13:38 +0300

    Add project files.

---
+++ WebApplication3/Models/ExamenDbSeeder.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace WebApplication3.Models
+{
+    public class ExamenDbSeeder
+    {
+        public static void Initialize(ExamenDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // Look for any movies.
+            if (context.Users.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Users.AddRange(
+                          new User
+                          {
+                              FirstName = "Corina",
+                              LastName = "Sofron",
+                              Username = "corina",
+                              Email = "corina@yahoo.com",
+                              Password = "123456",
+                              UserRole = UserRole.Admin,
+                              DataRegistered = System.DateTime.Now
+                          }
+                      );
+            context.SaveChanges();
+        }
+    }
+}
